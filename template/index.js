44bc960d95c15257dbc3b4fb336ec215779cba5f
Mateusz Swierczynski <m.swierczynski@smartrecruiters.com>
2023-02-03 15:20:24 +0100

    Test

--- template/index.js
+++ template/index.js
@@ -1,14 +1,12 @@
 import { File, Text } from "@asyncapi/generator-react-sdk";
 
 import { PackageDeclaration } from '../components/Common';
-import { javaPackageToPath } from '../utils/String.utils';
 import { AsyncApiController } from '../components/AsyncApiController';
 
 export default function({ asyncapi, params }) {
-    const filePath = `${javaPackageToPath(params.package)}`;
     const pkg = `${params.package}`;
     return (
-        <File name={filePath}>
+        <File name="AsyncApiController.java">
             <PackageDeclaration path={pkg} />
             <AsyncApiController />
         </File>
